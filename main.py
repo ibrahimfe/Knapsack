fd71aea21109efd97d07c40570137bd2a99295aa
ibrahimfe <ibrahiimm.f@gmail.com>
2023-12-29 21:07:00 +0700

    add test case

--- main.py
+++ main.py
@@ -2,12 +2,51 @@ from dynamic_programming import dynamic_knapsack
 from greedy_algorithm import knapsack_greedy
 from recursive_memoization import knapsack_recursive_memoization
 
-weights = [2, 3, 4, 5]
-values = [3, 4, 5, 6]
-capacity = 5
-greedy_result = knapsack_greedy(weights, values, capacity)
-dynamic_result = dynamic_knapsack(weights, values, capacity)
-recursive_result = knapsack_recursive_memoization(weights, values, capacity)
-print("Maximum Value:", greedy_result)
-print("Maximum Value:", dynamic_result)
-print("Maximum Value:", recursive_result)
+import unittest
+
+class TestKnapsackGreedy(unittest.TestCase):
+    def test_empty_list(self):
+        self.assertEqual(knapsack_greedy([], [], 10), 0, "Expected 0")
+        self.assertEqual(dynamic_knapsack([],[], 10), 0, "Expected 0")
+        self.assertEqual(knapsack_recursive_memoization([],[],10), 0, "Expected 0")
+
+    def test_single_item(self):
+        self.assertEqual(knapsack_greedy([1], [1], 10), 1, "Expected 1")
+        self.assertEqual(dynamic_knapsack([1], [1], 10), 1, "Expected 1")
+        self.assertEqual(knapsack_recursive_memoization([1], [1], 10), 1, "Expected 1")
+
+    def test_two_items(self):
+        self.assertEqual(knapsack_greedy([1, 2], [1, 2], 10), 3, "Expected 3")
+        self.assertEqual(dynamic_knapsack([1, 2], [1, 2], 10), 3, "Expected 3")
+        self.assertEqual(knapsack_recursive_memoization([1, 2], [1, 2], 10), 3, "Expected 3")
+
+    def test_three_items(self):
+        self.assertEqual(knapsack_greedy([1, 2, 3], [1, 2, 3], 10), 6, "Expected 6")
+        self.assertEqual(dynamic_knapsack([1, 2, 3], [1, 2, 3], 10), 6, "Expected 6")
+        self.assertEqual(knapsack_recursive_memoization([1, 2, 3], [1, 2, 3], 10), 6, "Expected 6")
+
+    def test_four_items(self):
+        self.assertEqual(knapsack_greedy([1, 2, 3, 4], [1, 2, 3, 4], 10), 10, "Expected 10")
+        self.assertEqual(dynamic_knapsack([1, 2, 3, 4], [1, 2, 3, 4], 10), 10, "Expected 10")
+        self.assertEqual(knapsack_recursive_memoization([1, 2, 3, 4], [1, 2, 3, 4], 10), 10, "Expected 10")
+
+    def test_capacity_exceeded(self):
+        self.assertEqual(knapsack_greedy([1, 2, 3, 4], [1, 2, 3, 4], 5), 3, "Expected 3")
+        self.assertEqual(dynamic_knapsack([1, 2, 3, 4], [1, 2, 3, 4], 5), 5, "Expected 5")
+        self.assertEqual(knapsack_recursive_memoization([1, 2, 3, 4], [1, 2, 3, 4], 5), 5, "Expected 5")
+
+    def test_random_inputs(self):
+        weights = [3, 2, 1, 4]
+        values = [10, 5, 2, 8]
+        capacity = 6
+        expected_result = 17
+        actual_result_greedy = knapsack_greedy(weights, values, capacity)
+        actual_result_dynamic = dynamic_knapsack(weights, values, capacity)
+        actual_result_recurive = knapsack_recursive_memoization(weights, values, capacity)
+        self.assertEqual(actual_result_greedy, expected_result, "Expected 17")
+        self.assertEqual(actual_result_dynamic, expected_result, "Expected 17")
+        self.assertEqual(actual_result_recurive, expected_result, "Expected 17")
+
+
+if __name__ == '__main__':
+    unittest.main()
